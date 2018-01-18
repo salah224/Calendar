369fe5ac9fef29335a7fef2ce85f59e8e8e3a115
salah225 <31827792+salah225@users.noreply.github.com>
2018-01-17 18:44:57 -0600

    Initial commit

---
+++ app/src/main/java/com/example/salah/calendar/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.salah.calendar;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    CalendarView calendarView;
+    Date date = new Date( );
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        CalendarView  calendarView =(CalendarView)findViewById( R.id.calendarView);
+        calendarView.setFocusedMonthDateColor( Color.YELLOW );
+        int focuseMonthDateColor = calendarView.getFocusedMonthDateColor();
+
+
+        calendarView.setOnDateChangeListener( new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth ) {
+
+                //int year, int month, int dayOfMonth
+
+                Toast.makeText( getApplicationContext(), dayOfMonth + "/ " + month + "/ "+ year + "\n " + date.toString(), Toast.LENGTH_LONG).show();
+               // Toast.makeText( getApplicationContext(), date.toString(),  Toast.LENGTH_LONG).show();
+            }
+        } );
+    }
+}
